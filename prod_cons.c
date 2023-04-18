a85c422a379e76f2522fce067d333dc41e92549f
millliono <aimiliosdraginis@gmail.com>
2023-04-19 01:34:28 +0300

    base form working

---
+++ prod_cons.c
@@ -0,0 +1,187 @@
+/*prod cons works with one of each thread type
+  necessary changes to the code
+  implemeted struct and load function
+  load function claculates 10 sine values
+  -Todo:
+      -use arguments in load function
+      -use more threads
+      -time
+      -remove sleep
+      -consumer infinite loop
+      -report
+
+*/
+
+//producer-consumer
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define QUEUESIZE 10
+#define LOOP 20
+
+void *producer (void *args);
+void *consumer (void *args);
+
+//*****************
+typedef struct {
+  void * (*work)();
+  // void * arg;
+} workFunction;
+
+void* work() { //TODO: use arg
+    // sum 100 sin values for no reason
+    float result = 0;
+    for (int i = 0; i < 100; i++) {
+        result = sin(i*3.14/180);
+    }
+    char* str = "calculated stuff";
+    void* ptr = (void*) str;
+    return ptr;
+}
+
+// initialize thread load 
+workFunction thread_load = { work };
+//**********************
+
+typedef struct {
+  workFunction buf[QUEUESIZE];
+  long head, tail;
+  int full, empty;
+  pthread_mutex_t *mut;
+  pthread_cond_t *notFull, *notEmpty;
+} queue;
+
+queue *queueInit (void);
+void queueDelete (queue *q);
+void queueAdd (queue *q, workFunction in);
+void queueDel (queue *q, workFunction *out);
+
+int main ()
+{
+  queue *fifo;
+  pthread_t pro, con;
+
+  fifo = queueInit ();
+  if (fifo ==  NULL) {
+    fprintf (stderr, "main: Queue Init failed.\n");
+    exit (1);
+  }
+  pthread_create (&pro, NULL, producer, fifo);
+  pthread_create (&con, NULL, consumer, fifo);
+  pthread_join (pro, NULL);
+  pthread_join (con, NULL);
+  queueDelete (fifo);
+
+  return 0;
+}
+
+void *producer (void *q)
+{
+  queue *fifo;
+  int i;
+
+  fifo = (queue *)q;
+
+  for (i = 0; i < LOOP; i++) {
+    pthread_mutex_lock (fifo->mut);
+    while (fifo->full) {
+      printf ("producer: queue FULL.\n");
+      pthread_cond_wait (fifo->notFull, fifo->mut);
+    }
+    queueAdd (fifo, thread_load);
+    pthread_mutex_unlock (fifo->mut);
+    pthread_cond_signal (fifo->notEmpty);
+    printf ("producer:  input item\n");
+    sleep (0);
+  }
+
+  return (NULL);
+}
+
+void *consumer (void *q)
+{
+  queue *fifo;
+  int i;
+  workFunction d;
+
+  fifo = (queue *)q;
+
+  for (i = 0; i < LOOP; i++) {
+    pthread_mutex_lock (fifo->mut);
+    while (fifo->empty) {
+      printf ("consumer: queue EMPTY.\n");
+      pthread_cond_wait (fifo->notEmpty, fifo->mut);
+    }
+    queueDel (fifo, &d);
+    pthread_mutex_unlock (fifo->mut);
+    pthread_cond_signal (fifo->notFull);
+    printf ("consumer:  %s\n", (char*) thread_load.work());
+    sleep(0);
+  }
+
+  return (NULL);
+}
+
+
+queue *queueInit (void)
+{
+  queue *q;
+
+  q = (queue *)malloc (sizeof (queue));
+  if (q == NULL) return (NULL);
+
+  q->empty = 1;
+  q->full = 0;
+  q->head = 0;
+  q->tail = 0;
+  q->mut = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+  pthread_mutex_init (q->mut, NULL);
+  q->notFull = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+  pthread_cond_init (q->notFull, NULL);
+  q->notEmpty = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+  pthread_cond_init (q->notEmpty, NULL);
+	
+  return (q);
+}
+
+void queueDelete (queue *q)
+{
+  pthread_mutex_destroy (q->mut);
+  free (q->mut);	
+  pthread_cond_destroy (q->notFull);
+  free (q->notFull);
+  pthread_cond_destroy (q->notEmpty);
+  free (q->notEmpty);
+  free (q);
+}
+
+void queueAdd (queue *q, workFunction in)
+{
+  q->buf[q->tail] = in;
+  q->tail++;
+  if (q->tail == QUEUESIZE)
+    q->tail = 0;
+  if (q->tail == q->head)
+    q->full = 1;
+  q->empty = 0;
+
+  return;
+}
+
+void queueDel (queue *q, workFunction *out)
+{
+  *out = q->buf[q->head];
+
+  q->head++;
+  if (q->head == QUEUESIZE)
+    q->head = 0;
+  if (q->head == q->tail)
+    q->empty = 1;
+  q->full = 0;
+
+  return;
+}
